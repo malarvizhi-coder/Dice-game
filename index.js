282312a55ca5011c1ace67638697e71f369df86a
malarvizhi-coder <74088895+malarvizhi-coder@users.noreply.github.com>
2023-04-18 22:04:58 +0530

    uploading scratch files

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var image1 = "images/dice"+ randomNumber1 +".png";
+var image2 = "images/dice"+ randomNumber2 +".png";
+
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML= "🌟Player 1 is the Winner🌟";
+}
+else if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML= "🌟It's a draw🌟";
+}
+else{
+    document.querySelector("h1").innerHTML= "🌟Player 2 is the Winner🌟";
+}
